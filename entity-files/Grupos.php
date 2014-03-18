ad5703479f29f21db92838354e6e269b55527cf6
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-18 14:39:25 -0300

    add propriedades da classe

--- entity-files/Grupos.php
+++ entity-files/Grupos.php
@@ -8,5 +8,24 @@
  */
 class Grupos extends AbstractModel
 {
+    /**
+     * Primary Key column.
+     *
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * Data de cadastro
+     * @Column(type="datetime")
+     */
+    protected $datcad;
+
+    /**
+     * @Column(type="string", length=100, nullable=true)
+     */
+    protected $descricao;
 
 }
