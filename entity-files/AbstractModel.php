2378b23a7a3d89b3a901b20e5987c44299b6190f
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-18 11:24:41 -0300

    add funcao find

--- entity-files/AbstractModel.php
+++ entity-files/AbstractModel.php
@@ -5,5 +5,19 @@
  */
 class AbstractModel
 {
+    /**
+     * Busca um registro na tabela tendo como base o parâmetro id
+     *
+     * @param $id
+     *
+     * @return AbstractModel
+     */
+    public static function find($id)
+    {
+        if (!$id) {
+            return null;
+        }
 
-} 
+        return EM::instance()->find(get_called_class(), $id);
+    }
+}
