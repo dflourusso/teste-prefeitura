91f40c07ccef015fccc40974849f2451bf0fa25b
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-18 15:33:52 -0300

    add propriedades do produto

--- entity-files/Produtos.php
+++ entity-files/Produtos.php
@@ -8,5 +8,40 @@
  */
 class Produtos extends AbstractModel
 {
+    /**
+     * Primary Key column.
+     *
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * Data de cadastro
+     * @Column(type="datetime")
+     */
+    protected $datcad;
+
+    /**
+     * @Column(type="string", length=100, nullable=true)
+     */
+    protected $descricao;
+
+    /**
+     * @Column(type="decimal")
+     */
+    protected $preco;
+
+    /**
+     * @Column(type="integer")
+     */
+    protected $grupo_id;
+
+    /**
+     * @ManyToOne(targetEntity="Grupos")
+     * @JoinColumn(name="grupo_id", referencedColumnName="id")
+     */
+    private $grupo;
 
 } 
