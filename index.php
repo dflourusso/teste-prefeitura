31dca97a4b588b5d2414d3ceb17a124337c6f50a
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-19 10:21:26 -0300

    add add produto

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 date_default_timezone_set('America/Sao_Paulo');
 require_once 'bootstrap.php';
 
-$grupo = new Grupos();
-$grupo->setDescricao('Alimentos');
-$grupo->setDatcad(new DateTime());
-$grupo->save();
 
-echo $grupo;
+//$grupo = new Grupos();
+//$grupo->setDescricao('Alimentos');
+//$grupo->setDatcad(new DateTime());
+//$grupo->save();
+//echo $grupo;
+
+
+$produto = new Produtos();
+$produto->setDatcad(new DateTime());
+$produto->setDescricao('Arroz');
+$produto->setPreco(5.5);
+$produto->setGrupo(Grupos::find(2));
+$produto->save();
+$produto->refresh();
+echo $produto;
