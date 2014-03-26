d99de4ba01ef3fec6770b4998bb81bedc692b0f7
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-26 13:45:04 -0300

    busca produtos de um grupo

--- index.php
+++ index.php
@@ -4,13 +4,9 @@ date_default_timezone_set('America/Sao_Paulo');
 require_once 'bootstrap.php';
 
 
-
-$f = Fornecedores::find(1);
-$p = $f->getProdutos();
-echo $p->count();
-echo PHP_EOL;
+$g = Grupos::find(2);
+$p = $g->getProdutos();
 foreach ($p as $v) {
     echo $v . PHP_EOL;
 }
-
 
