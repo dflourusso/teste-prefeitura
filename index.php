f014481609ada8ac225e8ec9d15f2e03105f79ba
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-26 10:39:47 -0300

    add insert in FaturamentosProdutos

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ date_default_timezone_set('America/Sao_Paulo');
 require_once 'bootstrap.php';
 
 
-$f = new Fornecedores();
-$f->setNomraz('Fornecetudo LTDA');
-$f->save();
+$fp = new FornecedoresProdutos();
+$fp->setProdutoId(1);
+$fp->setProduto(Produtos::find(1));
+$fp->setFornecedorId(1);
+$fp->setFornecedor(Fornecedores::find(1));
+$fp->save();
