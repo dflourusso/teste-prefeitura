08e78f7c6f8ff608cbe6c61b81639d61739bffe7
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-26 11:31:46 -0300

    add test para listar produtos de um fornecedor

--- index.php
+++ index.php
@@ -4,9 +4,13 @@ date_default_timezone_set('America/Sao_Paulo');
 require_once 'bootstrap.php';
 
 
-$fp = new FornecedoresProdutos();
-$fp->setProdutoId(1);
-$fp->setProduto(Produtos::find(1));
-$fp->setFornecedorId(1);
-$fp->setFornecedor(Fornecedores::find(1));
-$fp->save();
+
+$f = Fornecedores::find(1);
+$p = $f->getProdutos();
+echo $p->count();
+echo PHP_EOL;
+foreach ($p as $v) {
+    echo $v . PHP_EOL;
+}
+
+
