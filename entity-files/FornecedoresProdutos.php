ee63644755d58c3bd41fe173c7b7afbe3ece684b
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-26 09:54:06 -0300

    add propriedades to FornecedoresProdutos

--- entity-files/FornecedoresProdutos.php
+++ entity-files/FornecedoresProdutos.php
@@ -5,5 +5,32 @@
  */
 class FornecedoresProdutos
 {
+    /**
+     * Primary Key column.
+     *
+     * @Id
+     * @Column(type="integer")
+     */
+    protected $produto_id;
+
+    /**
+     * Primary Key column.
+     *
+     * @Id
+     * @Column(type="integer")
+     */
+    protected $fornecedor_id;
+
+    /**
+     * @ManyToOne(targetEntity="Produtos")
+     * @JoinColumn(name="produto_id", referencedColumnName="id")
+     */
+    private $produto;
+
+    /**
+     * @ManyToOne(targetEntity="Fornecedores")
+     * @JoinColumn(name="fornecedor_id", referencedColumnName="id")
+     */
+    private $fornecedor;
 
 } 
