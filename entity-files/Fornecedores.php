132a6e7542356a02033043f044b33f0db8853f3a
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-03-26 09:37:20 -0300

    add funcoes

--- entity-files/Fornecedores.php
+++ entity-files/Fornecedores.php
@@ -19,4 +19,40 @@ class Fornecedores
      */
     protected $nomraz;
 
+    /**
+     * Atribui valor para a propriedade id
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Obtém o valor da propriedade id
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Atribui valor para a propriedade nomraz
+     * @param string $nomraz
+     */
+    public function setNomraz($nomraz)
+    {
+        $this->nomraz = $nomraz;
+    }
+
+    /**
+     * Obtém o valor da propriedade nomraz
+     * @return string
+     */
+    public function getNomraz()
+    {
+        return $this->nomraz;
+    }
+
 } 
