a1aba66d127216c41a991cd75f07185ebb15ce41
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-04-01 14:37:42 -0300

    echo result arrayCollection

--- src/produtos.php
+++ src/produtos.php
@@ -1,9 +1,10 @@
 <?php
-$qb = EM::instance()->createQueryBuilder();
-$qb->select($qb->expr()->countDistinct('p.grupo_id'))->from('Produtos', 'p');
 
-$produtos = $qb->getQuery()->getArrayResult();
+$g = Grupos::find(1);
+$p = $g->getProdutos();
 
 echo "<pre>";
-var_export($produtos);
-echo "</pre>";
+foreach ($p as $k => $v) {
+    echo $v;
+}
+echo '</pre>';
