b27be5014228b4be15e00fce301abc62695e78ec
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-04-01 10:22:13 -0300

    add parameter

--- src/produtos.php
+++ src/produtos.php
@@ -1,7 +1,8 @@
 <?php
 $qb = EM::instance()->createQueryBuilder();
 $qb->select('p')->from('Produtos', 'p')
-    ->where('p.preco > 3');
+    ->where('p.preco > :preco')
+    ->setParameter('preco', 3);
 
 $produtos = $qb->getQuery()->getArrayResult();
 
