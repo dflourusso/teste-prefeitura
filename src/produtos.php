ba2470f9b6c28d3c8593f078d980a56e676cf614
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-04-01 11:01:37 -0300

    usando a expressao min

--- src/produtos.php
+++ src/produtos.php
@@ -1,6 +1,6 @@
 <?php
 $qb = EM::instance()->createQueryBuilder();
-$qb->select($qb->expr()->count('p'))->from('Produtos', 'p');
+$qb->select($qb->expr()->min('p.preco'))->from('Produtos', 'p');
 
 $produtos = $qb->getQuery()->getArrayResult();
 
