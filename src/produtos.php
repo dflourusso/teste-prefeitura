28424d99cae4b65e7c86c17f0d4084dcc038ae81
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-04-01 11:16:58 -0300

    utilizando o between

--- src/produtos.php
+++ src/produtos.php
@@ -1,7 +1,7 @@
 <?php
 $qb = EM::instance()->createQueryBuilder();
 $qb->select('p')->from('Produtos', 'p')
-    ->where($qb->expr()->in('p.id', array(6, 7, 9)));
+    ->where($qb->expr()->between('p.id', 7, 9));
 
 $produtos = $qb->getQuery()->getArrayResult();
 
