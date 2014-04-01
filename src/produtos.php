f7cde7d5dea9a2feb7aec1f355f95505bd446d1f
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-04-01 11:15:48 -0300

    utilizando in

--- src/produtos.php
+++ src/produtos.php
@@ -1,7 +1,7 @@
 <?php
 $qb = EM::instance()->createQueryBuilder();
 $qb->select('p')->from('Produtos', 'p')
-    ->where($qb->expr()->like('p.descricao', $qb->expr()->literal('%rod%')));
+    ->where($qb->expr()->in('p.id', array(6, 7, 9)));
 
 $produtos = $qb->getQuery()->getArrayResult();
 
