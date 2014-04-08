9837096470b8548e3b01a7ed01cfcda91a18c7d2
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-04-08 13:48:39 -0300

    add prorpiedade

--- entity-files/Grupos.php
+++ entity-files/Grupos.php
@@ -21,9 +21,15 @@ class Grupos extends AbstractModel
 
     /**
      * Data de cadastro
-     * @Column(type="datemesref")
+     * @Column(type="datetime")
      */
     protected $datcad;
+
+    /**
+     * Data de referencia
+     * @Column(type="datemesref")
+     */
+    protected $mesref;
 
     /**
      * @Column(type="string", length=100, nullable=true)
@@ -86,6 +92,22 @@ class Grupos extends AbstractModel
     public function getDatcad()
     {
         return $this->datcad;
+    }
+
+    /**
+     * @param DateTime $mesref
+     */
+    public function setMesref($mesref)
+    {
+        $this->mesref = $mesref;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getMesref()
+    {
+        return $this->mesref;
     }
 
     /**
